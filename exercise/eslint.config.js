2a78e2e81d0713f55ab6b8a7b368cbd46fef376b
fullness-MFurukawa <furukawa@fullness.co.jp>
2025-03-10 11:28:45 +0900

    ESlintの設定を追加

---
+++ exercise/eslint.config.js
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      parser: tsParser,
+      sourceType: "module",
+      ecmaVersion: "latest",
+      globals: {
+        console: "readonly",
+        require: "readonly",
+        exports: "readonly",
+        module: "readonly",
+        process: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+    },
+    rules: {
+      "no-undef": "off", 
+    },
+  },
+];
+
